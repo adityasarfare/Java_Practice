121fd39591bd9f49a103f07f888e1915f874cfd2
aditya <adityasarfare@gmail.com>
2024-12-21 13:54:06 +0000

    DP questions added

--- DynamicProgramming/src/main/java/com/example/dp/FrogJump3.java
+++ DynamicProgramming/src/main/java/com/example/dp/FrogJump3.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class FrogJump3 {
 
-	//Brute Force
+	//Memoization
   public int maxJump(int[] stones) {
         
 	  int n= stones.length;
-	  int dp[] = new int[n];
+	  int dp[] = new int[n+1];
 	  Arrays.fill(dp, -1);
 	  
 	  return dfs(n-1, stones, dp);
@@ -35,4 +35,45 @@ public class FrogJump3 {
 		}
 		return  dp[n] =Math.min(left, right);
 	}
+	
+	
+	//Tabulation
+//	public int maxJump(int[] stones) {
+//        int n = stones.length;
+//        int[] dp = new int[n];
+//      
+//      dp[0]=0;
+//
+//      for(int i=1; i< stones.length; i++){
+//        int firstJump= dp[i-1] + Math.abs(stones[i] - stones[i-1]);
+//        int secondJump = Integer.MAX_VALUE;
+//        if(i>1){
+//            secondJump = dp[i-2] + Math.abs(stones[i] - stones[i-2]);
+//        }
+//        dp[i] = Math.min(firstJump, secondJump);
+//      }
+//
+//      return dp[n-1];
+//}
+	
+	//public int maxJump(int[] stones) {
+//      int n = stones.length;
+//     
+//    
+//    int prev2=0;
+//    int prev=0;
+	
+//    for(int i=1; i< stones.length; i++){
+//      int firstJump= prev + Math.abs(stones[i] - stones[i-1]);
+//      int secondJump = Integer.MAX_VALUE;
+//      if(i>1){
+//          secondJump = prev2 + Math.abs(stones[i] - stones[i-2]);
+//      }
+//      int curri = Math.min(firstJump, secondJump);
+//	prev2 = prev;
+//	prev = curri;
+//    }
+//
+//    return prev;
+//}
 }
---
+++ DynamicProgramming/src/main/java/com/example/dp/FrogJumpTabulation4.java
@@ -0,0 +1,34 @@
+package com.example.dp;
+
+import java.util.Arrays;
+
+public class FrogJumpTabulation4 {
+
+	//Memoization
+  public static int maxJump(int[] stones, int k) {
+        
+	  int n= stones.length;
+	  int dp[] = new int[n];
+	  dp[0] =0;
+	  
+	  for(int i=1; i<n; i++) {
+		  
+		  int step =Integer.MAX_VALUE;
+		  for(int j=1; j<=k; j++){
+			  if(i-j>=0) {
+				  int jump = dp[i-j] + Math.abs(stones[i] - stones[i-j]);
+				  step = Math.min(step, jump);
+			  }
+			dp[i] = step;
+		  }
+	  }
+		return dp[n-1];
+	}
+	public static void main(String[] args) {
+		
+		int stones[] = { 30, 10, 60, 10, 60, 50 };
+		int k=2;
+	    System.out.println(maxJump(stones, k));
+	}
+	
+}
