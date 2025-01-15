6093d09788f08673886b865cd217912c7fafd4d7
aditya <adityasarfare@gmail.com>
2025-01-15 14:22:24 +0000

    Two-pointer questions added

--- LeetCodeSimple/src/main/java/com/example/App.java
+++ LeetCodeSimple/src/main/java/com/example/App.java
@@ -1,15 +1,59 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App {
+
+	 public List<List<Integer>> threeSum(int[] nums) {
+		 Arrays.sort(nums);
+		 
+		 List<List<Integer>> res = new ArrayList<>();
+		 for(int i=0; i<nums.length; i++) {
+			 if(nums[i]>0) {
+				 break;
+			 }
+			 if(i>0 && nums[i]==nums[i-1]) {
+				 continue;
+			 }
+			 
+			 int li =i+1;
+			 int hi = nums.length -1;
+			 
+			 while(li<=hi) {
+				 int sum = nums[i]+ nums[li] + nums[hi];
+				 
+				 if(sum<0) {
+					 li++;
+				 }
+				 else if(sum>0) {
+					 hi--;
+				 }
+				 else {
+					 if(sum ==0) {
+						res.add(Arrays.asList(nums[i], nums[li], nums[hi]));
+						li++;
+						hi--;
+						
+						while(li <hi && nums[i]==nums[i-1]) {
+							li++;
+						}
+					 }
+					 }
+			 }
+		 }
+		 
+		 return res;
+	 }
 
 
-
-
-{
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- LeetCodeSimple/src/main/java/com/example/TwoPointers/TwoIntegerSum2.java
+++ LeetCodeSimple/src/main/java/com/example/TwoPointers/TwoIntegerSum2.java
@@ -3,7 +3,7 @@ package com.example.TwoPointers;
 public class TwoIntegerSum2 {
 	public int[] twoSum(int[] numbers, int target) {
 		int li =0;
-		int hi = numbers.length;
+		int hi = numbers.length-1;
 		
 		int [] res = new int[2];
 		
